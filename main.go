dae417f47023a5e15c1373ab1ee5cf65fdf1242d
HiroyukiNakatsuma <nakatsu108@gmail.com>
2018-12-08 10:48:43 +0900

    [change]文言や記述順などの修正

--- main.go
+++ main.go
@@ -7,16 +7,17 @@ import (
     "os"
 )
 
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hello, %q", r.URL.Path[1:])
+}
+
 func main() {
     port := os.Getenv("ADVISER_GO_PORT")
+    fmt.Println(port)
     if port == "" {
-        log.Fatal("$PORT must be set")
+        log.Fatal("ADVISER_GO_PORT must be set")
     }
 
     http.HandleFunc("/", handler)
     http.ListenAndServe(":"+port, nil)
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello, %q", r.URL.Path[1:])
 }
