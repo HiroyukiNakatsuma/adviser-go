f3070cfe85c6cdf90e850e9716963983a50a2664
HiroyukiNakatsuma <nakatsu108@gmail.com>
2018-12-16 13:45:03 +0900

    [change] reply first greeting.

--- app/interface/controller/linebot_controller.go
+++ app/interface/controller/linebot_controller.go
@@ -10,7 +10,7 @@ func Reply(event *linebot.Event) (replyContent string) {
     if event.Type == linebot.EventTypeMessage {
         switch message := event.Message.(type) {
         case *linebot.TextMessage:
-            replyContent = usecase.ReplyEcho(message.Text)
+            replyContent = usecase.ReplyContent(message.Text)
         }
     }
 
--- app/usecase/linebot_usecase.go
+++ app/usecase/linebot_usecase.go
@@ -1,5 +1,33 @@
 package usecase
 
-func ReplyEcho(mes string) (reply string) {
-    return mes
+import (
+    "strings"
+    "math/rand"
+    "time"
+)
+
+const getFirstGreetingReply = "よろしくお願いします"
+
+func includeFirstGreeting(inputMes string) (includeFlag bool) {
+    var firstGreeting = [5]string{"よろしく", "よろしこ", "宜しく", "初めまして", "はじめまして"}
+    for _, g := range firstGreeting {
+        i := strings.Index(inputMes, g)
+        if i >= 0 {
+            return true
+        }
+    }
+    return false
+}
+
+func getFirstGreeting() (mes string) {
+    var emojiList = [8]string{"(^^)", "(^_^)", "(^-^)", "(*^^*)", "(^ ^)", "(^.^)", "(≧▽≦)", "！！"}
+    rand.Seed(time.Now().UnixNano())
+    return getFirstGreetingReply + emojiList[rand.Intn(8)]
+}
+
+func ReplyContent(inputMes string) (reply string) {
+    if includeFirstGreeting(inputMes) {
+        return getFirstGreeting()
+    }
+    return inputMes
 }
