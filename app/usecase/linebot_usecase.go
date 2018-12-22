4d6f189a4d89be2a89cf5dec3f32a7c872112a7a
HiroyukiNakatsuma <nakatsu108@gmail.com>
2018-12-22 11:42:46 +0900

    [change] ユーザー名の取得と表示

--- app/usecase/linebot_usecase.go
+++ app/usecase/linebot_usecase.go
@@ -6,7 +6,7 @@ import (
     "time"
 )
 
-const getFirstGreetingReply = "よろしくお願いします"
+const getFirstGreetingReply = "さん、よろしくお願いします"
 
 func includeFirstGreeting(inputMes string) (includeFlag bool) {
     var firstGreeting = [5]string{"よろしく", "よろしこ", "宜しく", "初めまして", "はじめまして"}
@@ -19,15 +19,15 @@ func includeFirstGreeting(inputMes string) (includeFlag bool) {
     return false
 }
 
-func getFirstGreeting() (mes string) {
+func getFirstGreeting(userName string) (mes string) {
     var emojiList = [8]string{"(^^)", "(^_^)", "(^-^)", "(*^^*)", "(^ ^)", "(^.^)", "(≧▽≦)", "！！"}
     rand.Seed(time.Now().UnixNano())
-    return getFirstGreetingReply + emojiList[rand.Intn(8)]
+    return userName + getFirstGreetingReply + emojiList[rand.Intn(8)]
 }
 
-func ReplyContent(inputMes string) (reply string) {
+func ReplyContent(inputMes string, userName string) (reply string) {
     if includeFirstGreeting(inputMes) {
-        return getFirstGreeting()
+        return getFirstGreeting(userName)
     }
     return inputMes
 }
