eeb9afaa6c0241b7b8c3544951a0ea1c576dfa81
HiroyukiNakatsuma <nakatsu108@gmail.com>
2019-03-09 09:52:10 +0900

    [change] restaurant extract three randomly.

--- app/usecase/service/restaurant_service.go
+++ app/usecase/service/restaurant_service.go
@@ -4,6 +4,7 @@ import (
     "github.com/HiroyukiNakatsuma/adviser-go/app/usecase/external_interface"
     "github.com/HiroyukiNakatsuma/adviser-go/app/usecase/presenter"
     "github.com/HiroyukiNakatsuma/adviser-go/app/domain/model"
+    "math/rand"
 )
 
 const isLunch = true
@@ -28,6 +29,24 @@ func (restServ *RestaurantService) ReplyContentByLocation(latitude float64, long
 }
 
 func (restServ *RestaurantService) getRestaurants(latitude float64, longitude float64, isLunch bool, isNoSmoking bool, amount int) string {
-    restaurants := restServ.restExServ.GetRestaurants(latitude, longitude, isLunch, isNoSmoking, amount)
-    return restServ.BuildReplyContent(restaurants)
+    externalRestaurants := restServ.restExServ.GetRestaurants(latitude, longitude, isLunch, isNoSmoking, amount)
+    rests := restServ.extractThreeRandomly(externalRestaurants)
+    return restServ.BuildReplyContent(rests)
+}
+
+func (restServ *RestaurantService) extractThreeRandomly(rests []*model.Restaurant) []*model.Restaurant {
+    shuffle(rests)
+    return rests[:2]
+}
+
+/**
+Fisher–Yates shuffle
+refs: https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
+ */
+func shuffle(data []*model.Restaurant) {
+    n := len(data)
+    for i := n - 1; i >= 0; i-- {
+        j := rand.Intn(i + 1)
+        data[i], data[j] = data[j], data[i]
+    }
 }
