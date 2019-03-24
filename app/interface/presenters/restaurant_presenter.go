5c5b874254d4da7351ec0de95383b47df6bec3b9
HiroyukiNakatsuma <nakatsu108@gmail.com>
2019-03-24 22:38:07 +0900

    [change] no image.

--- app/interface/presenters/restaurant_presenter.go
+++ app/interface/presenters/restaurant_presenter.go
@@ -7,6 +7,7 @@ import (
 )
 
 const noContentMessage = "ごめんなさい。該当するレストランがありませんでした。。"
+const noImageUrl = "https://shielded-tor-45730.herokuapp.com/images/give_up_240_240.jpg"
 const gnaviCreditText = "Supported by ぐるなびWebService"
 const altText = "This is restaurant list."
 const detailLabel = "詳細を見る"
@@ -25,8 +26,16 @@ func (restaurantPresenter *RestaurantPresenter) BuildReplyContent(rests []*model
     var columns []*linebot.CarouselColumn
     for _, rest := range rests {
         actions := linebot.NewURIAction(detailLabel, rest.Url)
-        columns = append(columns, linebot.NewCarouselColumn(rest.ImageUrl, rest.Name, gnaviCreditText, actions))
+        columns = append(columns, linebot.NewCarouselColumn(imageUrl(rest.ImageUrl), rest.Name, gnaviCreditText, actions))
     }
 
     return linebot.NewTemplateMessage(altText, linebot.NewCarouselTemplate(columns...))
+}
+
+func imageUrl(restaurantUrl string) string {
+    if restaurantUrl == "" {
+        return noImageUrl
+    } else {
+        return restaurantUrl
+    }
 }
